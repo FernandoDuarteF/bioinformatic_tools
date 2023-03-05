40ea2952cba6f74e71b2655366aaceaec482b446
FernandoDuarteF <d.fernando.fg@gmail.com>
2023-03-05 19:10:08 +0100

    Added option to parse a primer file

--- python/in_silico_PCR.py
+++ python/in_silico_PCR.py
@@ -7,19 +7,22 @@ try:
 except ImportError:
 	print("regex module required ($ pip install regex)")
 
-import regex
+#import regex
 
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-s","--sequence", help="sequence in fata format")
-	parser.add_argument("-f","--forward", help="forward primer", type = str)
-	parser.add_argument("-r","--reverse", help="reverse primer", type = str)
-	parser.add_argument("-m","--mismatches", default = 0, help="number of mismatches", type = str)
-	parser.add_argument("--minlength", default = 100, help="minimum product length", type = int)
-	parser.add_argument("--maxlength", default = 1000, help="maximum product length", type = int)
-	return parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-s","--sequence", help="sequence in fasta format")
+  parser.add_argument("-f","--forward", help="forward primer", type = str)
+  parser.add_argument("-r","--reverse", help="reverse primer", type = str)
+  parser.add_argument("-m","--mismatches", default = 0, help="number of mismatches", type = str)
+  parser.add_argument("--minlength", default = 100, help="minimum product length", type = int)
+  parser.add_argument("--maxlength", default = 1000, help="maximum product length", type = int)
+  parser.add_argument("-p","--primerfile", help="file with primers in csv format")
+  return parser.parse_args()
 
+#parser.add_argument("-p","--primerfile", help="file with primers in csv format")
+#return parser.parse_args()
 
 #regex.search would look for any of the words in [] in a string (e.g. IN [0]: re.search(r'[a-z]+', 'Hello')
 																#     OUT [0]: <re.Match object; span=(1, 5), match='ello'> (this is case sensitive))
@@ -37,178 +40,236 @@ def replace_ambiguity_codes(sequence):
 
 
 def revcom(sequence):
-	comp = {"A": "T", "C": "G", "G": "C", "T" :"A", "[":"]", "]":"[", "(":")", ")":"("}
-	rev = ""
-	for i in sequence:
-	        rev += i.replace(i, comp[i])
-	        rev_com = rev[::-1]
-	return rev_com
-
+  comp = {"A": "T", "C": "G", "G": "C", "T" :"A", "[":"]", "]":"[", "(":")", ")":"("}
+  rev = ""
+  for i in sequence:
+    rev += i.replace(i, comp[i])
+    rev_com = rev[::-1]
+  return rev_com
 
 if __name__ == "__main__":
+  
+  args = main()
 
-	args = main()
+  mis = args.mismatches
+  mismatches = f"{{s<={mis}}}" #Fuzzy matching (mismatch). From regex module f"{{s<={mis}}}" substitution
 
-	mis = args.mismatches
-	mismatches = f"{{s<={mis}}}" #Fuzzy matching (mismatch). From regex module f"{{s<={mis}}}" substitution
+  print(f"{mis} mismatches")
 
-	print(f"{mis} mismatches")
+  #Read sequence file
 
-	print("Checking primers...")
+  print("Reading sequence file...")
 
-	#Forward primer
+  sequence = args.sequence
 
-	#primerF = "ATG ATC GGA GCG GAC AAG"
-	primerF = args.forward
-	primerF = primerF.replace(" ", "")
-	primerF = [*primerF]
-	primerF = replace_ambiguity_codes(primerF)
-	primerF = f"({primerF})" #Brackets are necessary for fuzzy matching
+  fasta_file = open(sequence, "r")
+  num_lines = sum(1 for line in fasta_file)
+  fasta_file.close() #Look for a shortcut
 
-	print(f"Forward primer: {primerF}")
-	
-	patternF = regex.compile(primerF + mismatches)
+  fasta_file = open(sequence, "r")
+  seq_n, count = "", 0
+  seqs = []
 
-	#Forward primer reverse complement
+  for s in fasta_file:
+    s = s.strip()
+    count += 1
+    if s[0]==">":
+      if seq_n != "":
+        seqs.append([seq_n, seq])
+      seq_n, seq = s, ""
+    else:
+      seq += s
+      if count == num_lines:
+        seqs.append([seq_n, seq])
+        
+  print("Done")
 
-	primerFRv = revcom(primerF)
+  if args.primerfile is None:
 
-	patternFRv = regex.compile(primerFRv + mismatches)
+    print("Checking primers...")
 
-	#Reverse primer
+    #Forward primer
 
-	#primerR = "GCT CTT GAG GCA AGC ACT"
-	primerR = args.reverse
-	primerR = primerR.replace(" ", "")
-	primerR = [*primerR]
-	primerR = replace_ambiguity_codes(primerR)
-	primerR = f"({primerR})"
-	
-	print(f"Reverse primer: {primerR}")
+    primerF = args.forward
+    primerF = primerF.replace(" ", "")
+    primerF = [*primerF]
+    primerF = replace_ambiguity_codes(primerF)
+    primerF = f"({primerF})" #Brackets are necessary for fuzzy matching
 
-	patternR = regex.compile(primerR + mismatches)
+    print(f"Forward primer: {primerF}")
 
-	#Reverse primer reverse complement
+    patternF = regex.compile(primerF + mismatches)
 
-	primerRRv = revcom(primerR)
+	  #Forward primer reverse complement
 
-	patternRRv = regex.compile(primerRRv + mismatches)
+    primerFRv = revcom(primerF)
+    patternFRv = regex.compile(primerFRv + mismatches)
 
-	print("Done")
+	  #Reverse primer
 
-	#Read sequence file
+    primerR = args.reverse
+    primerR = primerR.replace(" ", "")
+    primerR = [*primerR]
+    primerR = replace_ambiguity_codes(primerR)
+    primerR = f"({primerR})"
 
-	print("Reading sequence file...")
+    print(f"Reverse primer: {primerR}")
 
-	sequence = args.sequence
+    patternR = regex.compile(primerR + mismatches)
 
-	fasta_file = open(sequence, "r")
+	  #Reverse primer reverse complement
 
-	num_lines = sum(1 for line in fasta_file)
-	
-	fasta_file.close() #Look for a shortcut
+    primerRRv = revcom(primerR)
+    patternRRv = regex.compile(primerRRv + mismatches)
 
-	fasta_file = open(sequence, "r")
+    print("Done")
 
-	seq_n, count = "", 0
-	seqs = []
-	for s in fasta_file:
-	    s = s.strip()
-	    count += 1
-	    if s[0]==">":
-	        if seq_n != "":
-	            seqs.append([seq_n, seq])
-	        seq_n, seq = s, ""
-	    else:
-	        seq += s
-	        if count == num_lines:
-	            seqs.append([seq_n, seq])
+    minlen = args.minlength
+    maxlen = args.maxlength
 
-	print("Done")
+    print(f"{minlen}bp minimum product length\n{maxlen}bp maximum product length")
 
-	minlen = args.minlength
-	maxlen = args.maxlength
+    print("Starting analysis...")
 
-	print(f"{minlen}bp minimum product length\n{maxlen}bp maximum product length")
-	
-	print("Starting analysis...")
-	
+    for a,b in seqs:
+      try:
+        matchF = patternF.finditer(b)
+        matchF = [[i.end() + 1, i.fuzzy_counts[0]] for i in matchF]
+        matchRRV = patternRRv.finditer(b)
+        matchRRV = [[i.start() - 1, i.fuzzy_counts[0]] for i in matchRRV]
+        matchFR = list(product(matchF, matchRRV))
+        for d,e in matchFR:
+          start = d[0]
+          end = e[0]
+          mis_F = d[1]
+          mis_R = e[1]
+          length = end - start
+          length = abs(length)
+          if length < maxlen and length > minlen:
+            print(a, start, mis_F, end, mis_R, length, sep = "\t")
 
-	for a,b in seqs:
-		try:
-			matchF = patternF.finditer(b)
-			matchF = [[i.end() + 1, i.fuzzy_counts[0]] for i in matchF]
-			matchRRV = patternRRv.finditer(b)
-			matchRRV = [[i.start() - 1, i.fuzzy_counts[0]] for i in matchRRV]
-			matchFR = list(product(matchF, matchRRV))
+	  		#For reverse complement, needs testing
 
-			for d,e in matchFR:
-				start = d[0]
-				end = e[0]
-				mis_F = d[1]
-				mis_R = e[1]
-				length = end - start
-				length = abs(length)
-				if length < maxlen and length > minlen:
-					print(a, start, mis_F, end, mis_R, length, sep = "\t")
+        matchFRv = patternFRv.finditer(b)
+        matchFRv = [[i.end() + 1, i.fuzzy_counts[0]] for i in matchFRv]
+        matchR = patternR.finditer(b)
+        matchR = [[i.start() - 1, i.fuzzy_counts[0]] for i in matchR]
+        matchFRRv = list(product(matchFRv, matchR))
+        for f,g in matchFRRv:
+          start = f[0]
+          end = g[0]
+          mis_F = f[1]
+          mis_R = g[1]
+          length = end - start
+          length = abs(length)
+          if length < maxlen and length > minlen:
+            print(a, start, mis_F, end, mis_R, length, sep = "\t")
+      except:
+        pass
+      
+  print("Done")
 
-			#For reverse complement, needs testing
-			matchFRv = patternFRv.finditer(b)
-			matchFRv = [[i.end() + 1, i.fuzzy_counts[0]] for i in matchFRv]
-			matchR = patternR.finditer(b)
-			matchR = [[i.start() - 1, i.fuzzy_counts[0]] for i in matchR]
-			matchFRRv = list(product(matchFRv, matchR))
+  if args.primerfile is not None:
 
-			for f,g in matchFRRv:
-				start = f[0]
-				end = g[0]
-				mis_F = f[1]
-				mis_R = g[1]
-				length = end - start
-				length = abs(length)
-				if length < maxlen and length > minlen:
-					print(a, start, mis_F, end, mis_R, length, sep = "\t")
-		except:
-			pass
+    with open(args.primerfile) as primers:
+      results = open("pcr_results.txt","w")
 
-	print("Done")
+      for lines in primers:
 
-	fasta_file.close()
+        lines = lines.replace(" ","")
+        lines = lines.split(",")
 
-	#print(match)
-#
-	#print(match.group(), match.fuzzy_counts[0], match.start(), match.end() + 1)
-		#print(seqs[0][0], seqs[0][1])
+        experiment = lines[0]
 
-#Fuzzy matching (mismatch). From regex module f"{{e<={mis}}}"
+        print (f"Experiment {experiment}")
 
-			#matchFRV = patternFRv.search(b)
-			#matchRRV = patternRRv.search(b)
-			#print(a, matchFRV.fuzzy_counts[0], matchFRV.end() + 1, matchRRV.fuzzy_counts[0], matchRRV.start() - 1, "RS", sep = "/t")
+        #Forward primer
 
-						#print(matchF)
-			#matchR = patternR.search(b)
-			#print(matchR)
-			#print(a, matchF.fuzzy_counts[0], matchF.end() + 1, matchRRV.fuzzy_counts[0], matchRRV.start() - 1, "FS")
-			#print(a, matchRRV.fuzzy_counts[0], matchRRV.start() - 1, "FS") #Fuzzy counts in this order: subs, indels and dels.
-			#matchFRV = patternFRv.search(b)
-			#matchRRV = patternRRv.search(b)
-			#print(a, matchFRV.fuzzy_counts[0], matchFRV.end() + 1, matchRRV.fuzzy_counts[0], matchRRV.start() - 1, "RS", sep = "/t")
+        primerF = lines[1]
+        primerF = [*primerF]
+        primerF = replace_ambiguity_codes(primerF)
+        primerF = f"({primerF})" #Brackets are necessary for fuzzy matching
+
+        print(f"Forward primer: {primerF}")
+
+        patternF = regex.compile(primerF + mismatches)
+
+	      #Forward primer reverse complement
+
+        primerFRv = revcom(primerF)
+        patternFRv = regex.compile(primerFRv + mismatches)
+
+        #Reverse primer
+
+        primerR = lines[2]
+        primerR = [*primerR]
+        primerR = replace_ambiguity_codes(primerR)
+        primerR = f"({primerR})"
+
+        print(f"Reverse primer: {primerR}")
+
+        patternR = regex.compile(primerR + mismatches)
+
+	      #Reverse primer reverse complement
+
+        primerRRv = revcom(primerR)
+        patternRRv = regex.compile(primerRRv + mismatches)
+
+        print("Done")
+
+        minlen = lines [3]
+        minlen = int(minlen)
+        maxlen = lines [4]
+        maxlen = int(maxlen)
+
+        print(f"{minlen}bp minimum product length")
+        print(f"{maxlen}bp maximum product length")
+
+        print("Starting analysis...")
+
+        for a,b in seqs:
+          try:
+            matchF = patternF.finditer(b)
+            matchF = [[i.end() + 1, i.fuzzy_counts[0]] for i in matchF]
+            matchRRV = patternRRv.finditer(b)
+            matchRRV = [[i.start() - 1, i.fuzzy_counts[0]] for i in matchRRV]
+            matchFR = list(product(matchF, matchRRV))
+            for d,e in matchFR:
+              start = d[0]
+              end = e[0]
+              mis_F = d[1]
+              mis_R = e[1]
+              length = end - start
+              length = abs(length)
+              if length < maxlen and length > minlen:
+                print(experiment, a, start, mis_F, end, mis_R, length, sep = "\t", file=results)
+                #results.write(experiment, a, start, mis_F, end, mis_R, length, sep = "\t")
+
+	  		    #For reverse complement, needs testing
+
+            matchFRv = patternFRv.finditer(b)
+            matchFRv = [[i.end() + 1, i.fuzzy_counts[0]] for i in matchFRv]
+            matchR = patternR.finditer(b)
+            matchR = [[i.start() - 1, i.fuzzy_counts[0]] for i in matchR]
+            matchFRRv = list(product(matchFRv, matchR))
+            for f,g in matchFRRv:
+              start = f[0]
+              end = g[0]
+              mis_F = f[1]
+              mis_R = g[1]
+              length = end - start
+              length = abs(length)
+              if length < maxlen and length > minlen:
+                print(experiment, a, start, mis_F, end, mis_R, length, sep = "\t", file=results)
+                #results.write(experiment, a, start, mis_F, end, mis_R, length, sep = "\t")
+          except:
+            pass
+      
+      results.close()
+
+      print("Done")
 
 
-	#a = [[i.group(), i.end(), i.fuzzy_counts[0]] for i in matchF]
-#
-	#b = [[i.group(), i.start(), i.fuzzy_counts[0]] for i in matchR]
-#
-	#c = list(product(a,b))
-#
-	#for a, b in c:
-	#	start = a[1]
-	#	end = b[1]
-	#	group = a[0]
-	#	length = start - end
-	#	length = abs(length)
-	#	print(length)
-	#	if length < 20 and length > 10:
-	#		print(group, start, end, length)
-	#	
+        #print(experiment, primerR, primerF, minlen, maxlen)
+  
+  fasta_file.close()
